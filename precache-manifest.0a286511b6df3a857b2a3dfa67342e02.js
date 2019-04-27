c9b725b2b044068792e6ef87c152f4479a0afac3
Shante Austin <shante.austin19@gmail.com>
2019-04-27 00:31:00 -0400

    Updates

---
+++ precache-manifest.0a286511b6df3a857b2a3dfa67342e02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "751aa55643e6b4e4a892",
+    "url": "/React-Flashcards/static/css/main.fa5cdd8e.chunk.css"
+  },
+  {
+    "revision": "751aa55643e6b4e4a892",
+    "url": "/React-Flashcards/static/js/main.d30837cb.chunk.js"
+  },
+  {
+    "revision": "68ba95aaa7230f3f38fd",
+    "url": "/React-Flashcards/static/js/runtime~main.8ffe6932.js"
+  },
+  {
+    "revision": "87537367b0f7417be8be",
+    "url": "/React-Flashcards/static/js/2.fc35b867.chunk.js"
+  },
+  {
+    "revision": "1338451c19ef9195f5412a6c323cf538",
+    "url": "/React-Flashcards/index.html"
+  }
+];
